530e1089da4d5e1906ec22cfa599945d94bb637c
kyh <mse1520@naver.com>
2021-08-03 22:40:10 +0900

    Add WebSocketStomp

--- webSocketTestConsole/Program.cs
+++ webSocketTestConsole/Program.cs
@@ -1,9 +1,13 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Net.WebSockets;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace webSocketTestConsole
 {
@@ -11,84 +15,149 @@ namespace webSocketTestConsole
     {
         static void Main(string[] args)
         {
-            ClientWebSocket ws = new ClientWebSocket();
+
             Uri uri = new Uri("ws://localhost:8080/ws/websocket");
+            WebSocketStomp webSocketStomp = new WebSocketStomp(uri);
 
-            ws.ConnectAsync(uri, CancellationToken.None);
-            Thread.Sleep(2000);
-
-            Console.WriteLine(ws.State); // open
-
-            receiveMsg(ws, (msg) => {
-                Console.WriteLine(msg);
+            webSocketStomp.onMessage((header, data) => {
+                Console.WriteLine(data);
             });
 
-            var connectBuffer = new StringBuilder();
-            var subscribeBuffer = new StringBuilder();
-            var sendBuffer = new StringBuilder();
+            webSocketStomp.subscribSync("/topic/request/scaleData");
 
-            connectBuffer.Append("CONNECT\n");
-            connectBuffer.Append("content-length:0\n");
-            connectBuffer.Append("accept-version:1.2\n");
-            connectBuffer.Append("host:edge\n");
-            connectBuffer.Append("\n");
-            connectBuffer.Append("\0");
+            var data = new JObject();
+            data.Add("command", "SEND");
+            data.Add("message", "I love JH!!!");
+            var dataText = JsonConvert.SerializeObject(data);
 
-            subscribeBuffer.Append("SUBSCRIBE\n");
-            subscribeBuffer.Append("content-length:0\n");
-            subscribeBuffer.Append("id:sub-0\n");
-            subscribeBuffer.Append("destination:/topic/request/scaleData\n");
-            subscribeBuffer.Append("\n");
-            subscribeBuffer.Append("\0");
-
-            sendBuffer.Append("SEND\n");
-            sendBuffer.Append("content-length:52\n");
-            sendBuffer.Append("id:sub-0\n");
-            sendBuffer.Append("destination:/request/scaleData\n");
-            sendBuffer.Append("\n");
-            sendBuffer.Append("{\"Subject\":\"Stomp client\",\"Message\":\"Hello World!!\"}");
-            sendBuffer.Append("\0");
-            
-            ws.SendAsync(Encoding.UTF8.GetBytes(connectBuffer.ToString()), WebSocketMessageType.Text, true, CancellationToken.None);
-            Thread.Sleep(1000);
-            ws.SendAsync(Encoding.UTF8.GetBytes(subscribeBuffer.ToString()), WebSocketMessageType.Text, true, CancellationToken.None);
-            Thread.Sleep(1000);
-            ws.SendAsync(Encoding.UTF8.GetBytes(sendBuffer.ToString()), WebSocketMessageType.Text, true, CancellationToken.None);
-            Thread.Sleep(1000);
-
-            
-
-            //byte[] receiveMsg = new byte[1000];
-            //ws.ReceiveAsync(receiveMsg, CancellationToken.None);
-            //Thread.Sleep(1000);
-            //Console.WriteLine(Encoding.Default.GetString(receiveMsg));
-            //Console.WriteLine("receive end!!");
-            //ws.ReceiveAsync(receiveMsg, CancellationToken.None);
-            //Thread.Sleep(1000);
-            //Console.WriteLine(Encoding.Default.GetString(receiveMsg));
-            //Console.WriteLine("receive end!!");
+            webSocketStomp.sendSync("/request/scaleData", dataText);
 
             Console.ReadLine();
         }
 
-        private static async void receiveMsg(ClientWebSocket ws, Action<string> callback)
+        
+    }
+
+    public class WebSocketStomp
+    {
+        #region ★ 멤버 변수
+        private Guid uuid = Guid.NewGuid();
+        private ClientWebSocket ws = new ClientWebSocket();
+        // task 취소 토큰
+        private CancellationTokenSource cts = new CancellationTokenSource();
+
+        public int timeout = 5000;
+        #endregion
+
+        #region ★ 생성자 & 소멸자
+        public WebSocketStomp(Uri uri)
+        {
+            connectSync(uri);
+        }
+
+        ~WebSocketStomp()
+        {
+            cts.Cancel();
+            cts.Dispose();
+            ws.Dispose();
+        }
+        #endregion
+
+        #region ★ 동기 실행
+        private void connectSync(Uri uri)
+        {
+            ws.ConnectAsync(uri, cts.Token).Wait(timeout);
+            
+            var buf = new StringBuilder();
+
+            buf.Append("CONNECT\n");
+            buf.Append("content-length:0\n");
+            buf.Append("accept-version:1.2\n");
+            buf.Append("host:edge\n");
+            buf.Append("\n");
+            buf.Append("\0");
+
+            ws.SendAsync(Encoding.UTF8.GetBytes(buf.ToString()), WebSocketMessageType.Text, true, cts.Token).Wait(timeout);
+        }
+
+        public void subscribSync(string destination)
+        {
+            var buf = new StringBuilder();
+
+            buf.Append("SUBSCRIBE\n");
+            buf.Append("content-length:0\n");
+            buf.Append($"id:sub-{uuid}\n");
+            buf.Append($"destination:{destination}\n");
+            buf.Append("\n");
+            buf.Append("\0");
+
+            ws.SendAsync(Encoding.UTF8.GetBytes(buf.ToString()), WebSocketMessageType.Text, true, cts.Token).Wait(timeout);
+        }
+
+        public void sendSync(string destination, string message)
+        {
+            var buf = new StringBuilder();
+
+            buf.Append("SEND\n");
+            buf.Append($"content-length:{message.Length}\n");
+            buf.Append($"destination:{destination}\n");
+            buf.Append("\n");
+            buf.Append(message);
+            buf.Append("\0");
+
+            ws.SendAsync(Encoding.UTF8.GetBytes(buf.ToString()), WebSocketMessageType.Text, true, cts.Token).Wait(timeout);
+        }
+        #endregion
+
+        #region ★ 비동기 실행
+        public void connectAsync(Uri uri)
+        {
+            Task.Run(() => {
+                connectSync(uri);
+            });
+        }
+
+        public void subscribAsync(string destination)
+        {
+            Task.Run(() => {
+                subscribSync(destination);
+            });
+        }
+
+        public void sendAsync(string destination, string message)
+        {
+            Task.Run(() => {
+                sendSync(destination, message);
+            });
+        }
+        #endregion
+
+        #region ★ 이벤트
+        public async void onMessage(Action<string, string> callback)
         {
             string result = "";
             byte[] receiveMsg = new byte[1];
 
             while (true)
             {
-                await ws.ReceiveAsync(receiveMsg, CancellationToken.None);
+                await ws.ReceiveAsync(receiveMsg, cts.Token);
                 var character = Encoding.Default.GetString(receiveMsg);
 
                 result += character;
 
                 if (character == "\0")
                 {
-                    callback(result);
+                    var splitResult = result.Split('\n');
+                    var data = splitResult[splitResult.Length - 1];
+                    var header = "";
+                    for (var i = 0; i < splitResult.Length - 1; i++)
+                        header += $"{splitResult[i]}\n";
+
+                    callback(header, data);
                     result = "";
                 }
             }
         }
+        #endregion
     }
 }
